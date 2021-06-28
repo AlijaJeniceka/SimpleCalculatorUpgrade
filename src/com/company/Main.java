ce58d784182b70d65658dd528d035b9aa0c2dba5
AlijaJeniceka <alija.juldaseva@gmail.com>
2021-06-28 11:02:51 +0300

    Second version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,47 +3,66 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        double firstValue;
-        double secondValue;
-        double answer;
+        int firstValue;
+        int secondValue;
+        int answer;
 
         //initialize variables
         firstValue = 22;
         secondValue = 7;
         answer = 0;
 
-        String operator = "p";
-
-        System.out.println(doArithmetic(firstValue, secondValue, operator));
+        //      String operator = "p";
+        int operator = Math.max(firstValue, secondValue);
+        //       System.out.println(doArithmetic(firstValue, secondValue, operator));
+        System.out.println(doMinMax(firstValue, secondValue, operator));
     }
 
-    public static double doArithmetic(double firstNumber, double secondNumber, String operator) {
-        double answer;
-        answer = 0;
-        switch (operator) {
-            case "+":
-                answer = firstNumber + secondNumber;
-                //   System.out.println(answer)
-                return answer;
-            case "-":
-                answer = firstNumber - secondNumber;
-                //   System.out.println(answer)
-                return answer;
-            case "*":
-                answer = firstNumber * secondNumber;
-                //   System.out.println(answer)
-                return answer;
-            case "/":
-                answer = firstNumber / secondNumber;
-                //   System.out.println(answer);
-                return answer;
-
-            default:
-                System.out.println("Enter a valid operator");
+        public static double doMinMax (int firstValue, int secondValue, int operator){
+            double answer;
+            answer = 0;
+            switch (operator) {
+                case Math.max(firstValue, secondValue):
+                    return answer;
+                    break;
+                case Math.min(firstValue, secondValue):
+                    return answer;
                 break;
+                default:
+                    System.out.println("Enter numbers");
+                    break;
+            }
+            return answer;
         }
-        return answer;
-        //Create a method that checks two numbers and uses a switch to return max, min on that number.
+
+    //public static double doArithmetic(double firstNumber, double secondNumber, String operator) {
+      //  double answer;
+     //   answer = 0;
+      //  switch (operator) {
+      //      case "+":
+      //          answer = firstNumber + secondNumber;
+                //   System.out.println(answer)
+      //          return answer;
+      //      case "-":
+      //          answer = firstNumber - secondNumber;
+                //   System.out.println(answer)
+      //          return answer;
+       //     case "*":
+       //         answer = firstNumber * secondNumber;
+                //   System.out.println(answer)
+       //         return answer;
+       //     case "/":
+         //       answer = firstNumber / secondNumber;
+                //   System.out.println(answer);
+         //       return answer;
+
+        //    default:
+         //       System.out.println("Enter a valid operator");
+         //       break;
+       // }
+      //  return answer;
+        //Create.
+        // a method that checks two numbers and uses a switch to return max, min on that number.
+
     }
 
-}
